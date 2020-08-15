43a2d5aff74b40dae48c997046f0724825bd54f7
Елиа <50628441+EliaMladenova@users.noreply.github.com>
2020-08-15 10:23:05 +0300

    add arrays.js

---
+++ basics/11.arrays.js
@@ -0,0 +1,27 @@
+//Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+console.log(names[2]);
+console.log(names.length);
+
+//Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
+
+//Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('blue');
+john.unshift('Mr');
+console.log(john);
+john.pop();
+console.log(john);
+john.shift();
+console.log(john);
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is not a designer' : 'John is a designer';
+
+console.log(isDesigner);
